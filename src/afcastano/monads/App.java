35382fb1b915c2a99cc5e22fec751225d92a477f
Andres Castano <andres.castano@papercut.com>
2016-07-06 00:58:56 +1000

    Initial commit

---
+++ src/afcastano/monads/App.java
@@ -0,0 +1,79 @@
+package afcastano.monads;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class App {
+
+    DomainLogic domainLogic;
+
+    public App() {
+        domainLogic = new DomainLogic();
+    }
+
+    public void exec(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number:\t");
+        Optional<Integer> firstVal = readIntSafe(scanner);
+
+        System.out.print("Enter second number:\t");
+        Optional<Integer> secondVal = readIntSafe(scanner);
+
+        System.out.print("Enter third number:\t");
+        Optional<Integer> thirdVal = readIntSafe(scanner);
+
+        Optional<Double> result = doLogic(firstVal, secondVal, thirdVal);
+
+        printResult(result);
+    }
+
+    private Optional<Double> doLogic(Optional<Integer> firstVal,
+                                     Optional<Integer> secondVal,
+                                     Optional<Integer> thirdVal) {
+
+        if(!firstVal.isPresent() || !secondVal.isPresent() || !thirdVal.isPresent()) {
+            return Optional.empty();
+        }
+
+        if(thirdVal.get() < 0) {
+            return Optional.empty();
+        }
+
+        Double result = domainLogic.calculate(firstVal.get(), secondVal.get(), thirdVal.get());
+
+        return Optional.of(result);
+    }
+
+    public static void main (String[] args) {
+        new App().exec();
+    }
+
+    private static void printResult(Optional<Double> result) {
+
+        if (result.isPresent()) {
+            System.out.println("Great, the result is: " + result);
+
+        } else {
+            System.out.println("Ups something went wrong");
+
+        }
+    }
+
+    private Optional<Integer> readIntSafe(Scanner scanner) {
+        Optional<Integer> firstVal;
+
+        try {
+            firstVal = Optional.of(scanner.nextInt());
+
+        } catch (Exception e) {
+            firstVal = Optional.empty();
+
+        }
+
+        scanner.nextLine();
+        return firstVal;
+    }
+
+
+}
---
+++ src/afcastano/monads/DomainLogic.java
@@ -0,0 +1,10 @@
+package afcastano.monads;
+
+public class DomainLogic {
+
+    public Double calculate(int value1, int value2, int value3) {
+        return Double.valueOf(value1 + value2 / value3);
+
+    }
+
+}
